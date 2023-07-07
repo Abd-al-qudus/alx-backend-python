6f481aa4c8e931dee0f41dfff421bc775a51c017
Abd-al-qudus <Abd-al-qudus>
2023-07-07 22:08:38 +0000

    fixed mypy validation error

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,6 +8,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """returns a list of type any from the tuple"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
