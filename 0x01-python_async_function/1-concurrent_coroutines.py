9fdd3feb31aaf5bd3f47bceaa657916a11fcd2b6
Abd-al-qudus <Abd-al-qudus>
2023-07-10 23:54:48 +0000

    fixed random wait typo

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,14 @@
     2 int arguments (in this order): n and max_delay
     and spawn the wait_random function using the max_delay param"""
 
-
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random for n times and returns a list of floats"""
-    return [await wait_random(max_delay) for _ in range(n)]
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
