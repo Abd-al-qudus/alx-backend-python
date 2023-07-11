16e18b7b8968f1967e7bd43d851f56beb1ba83e5
Abd-al-qudus <Abd-al-qudus>
2023-07-11 20:08:15 +0000

    async comprehension corrected 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,11 +12,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """async coroutine that measures the rumtime of parallel comprehensions"""
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
     stop = time.perf_counter()
     return stop - start
